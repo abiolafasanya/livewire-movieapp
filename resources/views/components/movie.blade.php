187e0eb40bab504ccf0fab0c48b5c6e09aa12de8
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-06-18 07:08:58 +0100

    update

--- resources/views/components/movie.blade.php
+++ resources/views/components/movie.blade.php
@@ -13,25 +13,34 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script> --}}
+
         @livewireStyles
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="bg-gray-900 text-gray-100">
-        <nav class="border-b border-gray-800">
-            <div class="container mx-auto flex flex-col md:flex-row justify-between items-center px-8 py-6">
+        <nav class="border-b border-gray-800 md:border-gray-700 sm:relative">
+
+            <div 
+                class="container mx-auto flex flex-col md:flex-row justify-between items-center px-8 py-6">
                 <div class="flex flex-col md:flex-row items-center">
-                    <a href="{{ route('movies') }}">
+                    <span class="flex flex-row justify-between items-center mr-2">
                         <svg class="bg-gray-100 mx-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"/>
                         </svg>
-                    </a>
-                    <a href="{{ route('movies') }}" class="text-gray-300 md:ml-2 hover:text-gray-100">Movies</a>
-                    <a href="" class="text-gray-300 md:ml-2 hover:text-gray-100">Tv shows</a>
-                    <a href="" class="text-gray-300 md:ml-2 hover:text-gray-100">Actors</a>
-                   
+                        <a href="{{ route('movies') }}" class="text-2xl">BeeMovie</a>
+                    </span>
+                    
+                    <span class="flex items-center flex-col md:flex-row" x-show="isOpen">
+                        <a href="{{ route('movies') }}" class="text-gray-400 md:ml-2 hover:text-gray-100">Movies</a>
+                        <a href="" class="text-gray-400 md:ml-2 hover:text-gray-100">Tv shows</a>
+                        <a href="" class="text-gray-400 md:ml-2 hover:text-gray-100">Actors</a>
+                    </span>
+                    
                 </div>
+
                 <div class="flex flex-col md:flex-row items-center">
                    <div class="relative md:mt-0 mt-2">
                         <input type="text" class="bg-gray-800 pl-10 text-sm focus:border-gray-500 focus:outline-none rounded-full w-64 px-4 py-1" placeholder="Search">
@@ -48,9 +57,11 @@
                            <img src="{{ asset('img/man.png') }}" alt="avatar" class="rounded-full w-8 h-8">
                        </a>
                    </div>
+    
                 </div>
 
             </div>
+            
         </nav>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
