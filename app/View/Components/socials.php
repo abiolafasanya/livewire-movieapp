52b08e780e42c5d5dc09e9434891cec208802048
fastbeetech <hezekiahfasanya@gmail.com>
2022-08-28 22:26:48 +0100

    commit

--- app/View/Components/socials.php
+++ app/View/Components/socials.php
@@ -14,13 +14,13 @@ class socials extends Component
      */
     public function __construct($socials, $actor)
     {
-        $socials = collect($socials)->merge([
-            'facebook' => $socials['facebook_id'] ? 'https://facebook.com/'.$socials['facebook_id'] : null,
-            'twitter' => $socials['twitter_id'] ? 'https://twitter.com/'.$socials['twitter_id'] : null,
-            'instagram' => $socials['instagram_id'] ? 'https://instagram.com/'.$socials['instagram_id'] : null,
-        ])->only([
-            'facebook', 'twitter', 'instagram'
-        ]);
+        // $socials = collect($socials)->merge([
+        //     'facebook' => $socials['facebook_id'] ? 'https://facebook.com/'.$socials['facebook_id'] : null,
+        //     'twitter' => $socials['twitter_id'] ? 'https://twitter.com/'.$socials['twitter_id'] : null,
+        //     'instagram' => $socials['instagram_id'] ? 'https://instagram.com/'.$socials['instagram_id'] : null,
+        // ])->only([
+        //     'facebook', 'twitter', 'instagram'
+        // ]);
         $this->socials = $socials;
         $this->actor = $actor;
     }
--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -1,13 +1,13 @@
 <div class="flex space-x-4 items-center mt-2">
-    @if ($socials['facebook'])
+    @if ($socials['facebook_id'])
         {{-- <a href="{{$socials['facebook_id']}}"><i class="fab fa-facebook-square text-xl" aria-hidden="true"></i></a> --}}
-        <a href="{{$socials['facebook']}}"><i class="fab fa-facebook-square text-xl" aria-hidden="true"></i></a>
+        <a href="{{$socials['facebook_id']}}"><i class="fab fa-facebook-square text-xl" aria-hidden="true"></i></a>
     @endif
-    @if ($socials['twitter'])
-        <a href="{{$socials['twitter']}}"><i class="fab fa-twitter text-xxl" aria-hidden="true"></i></a>
+    @if ($socials['twitter_id'])
+        <a href="{{$socials['twitter_id']}}"><i class="fab fa-twitter text-xxl" aria-hidden="true"></i></a>
     @endif
-    @if ($socials['instagram'])
-    <a href="{{$socials['instagram']}}"><i class="fab fa-instagram text-xl" aria-hidden="true"></i></a>
+    @if ($socials['instagram_id'])
+    <a href="{{$socials['instagram_id']}}"><i class="fab fa-instagram text-xl" aria-hidden="true"></i></a>
     @endif
     @if ($actor['homepage'])
     <a href="{{$actor['homepage']}}"><i class="fas fa-globe text-xl" aria-hidden="true"></i></a>
